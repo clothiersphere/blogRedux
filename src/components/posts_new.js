2636bf18c46810d2fe193f290e483823bbfce48d
Spacemixup <xchrislee@gmail.com>
2016-04-25 19:53:25 -0700

    Add fields using redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
 
+//near identical to { connect }
+import { reduxForm } from 'redux-form';
+
 class PostsNew extends Component {
 	render () {
+		// const handleSubmit = this.props.handleSubmit;
+		// const title = this.props.fields.title;
+
+		// {...title} means that all of the properties on the title object show up in the input
+		//<input type="text" className="form-control" onChange={title.onChange} onUpdate={title.onUpdate}/>
+
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
+
 	 	return (	
-	 		<div> 
-		 		<form>
-	 			<input>
-	 			</input>
-	 			<input>
-	 			</input>
-	 			<input>
-	 			</input>
+		 	<form onSubmit={handleSubmit}>
+		 		<h3>Create A New Post</h3>
+		 		<div className="form-group">
+		 			<label>Title</label>	
+		 			<input type="text" className="form-control" {...title} />
+		 		</div>
+		 		
+		 		<div className="form-group">
+		 			<label>Categories</label>	
+		 			<input type="text" className="form-control" {...categories} />
+		 		</div>
+		 		
+		 		<div className="form-group">
+		 			<label>Content</label>	
+		 			<textarea className="form-control" {...content} />
+		 		</div>
+		 		
+		 		<button type="submit" className="btn btn-primary">Submit</button>
 	 		</form>
-		 	</div>
 	 	)
 	}
 }
 
-export default PostsNew;
+//handles state and injects helpers into this.props component
+export default reduxForm({ 
+	form: 'PostsNewForm',
+	fields: ['title', 'categories', 'content']
+})(PostsNew);
+
+//user types something in ...record it on application state
+//state === { 
+//	form: {
+			// PostsNewForm: {
+				// title: '.....'
+				// categories: '.....'
+				// content: '.....'
+			// }
+// }
+//}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts.js'
 
+//import redux-form - grab reducer roperty and create a variable named formReducer
+import { reducer as formReducer } from 'redux-form';
+
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
