d05d59716ad06492a9ea085f8bf3ac5d1c240bf2
Spacemixup <xchrislee@gmail.com>
2016-05-04 18:32:33 -0700

    Add delete feature that redirect to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 //the id is coming from the url - pull the id from url - pass it to fetchPost 
 // fetchPost makes back end request - resolves w/ data reducer picks it up
 
 class PostsShow extends Component { 
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id);
+	}
+
+	onDeleteClick() {
+		this.props.deletePost(this.props.params.id)
+			.then(() => { this.context.router.push('/'); });
 	}
 
 	render() {
@@ -20,6 +30,12 @@ class PostsShow extends Component {
 
 		return (
 			<div>
+				<Link to="/">Back to Index</Link>
+				<button 
+					className="btn btn-danger pull-xs-right"
+					onClick={this.onDeleteClick.bind(this)}>
+					Delete Post
+				</button>
 				<h3>{post.title}</h3>
 				<h3>Categories: {post.categories}</h3>
 				<p>{post.content}</p>
@@ -35,4 +51,4 @@ function mapStateToProps(state) {
 }
 
 // export default connect(null, { fetchPost })(PostsShow);
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
